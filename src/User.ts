0fdc5971812bb44be6e857c5b6443e16bbce546f
Arthur Vinicius Ribeiro Silva <arthur.ribeiro_badaro@prestador.globo>
2023-05-23 18:08:46 -0300

    feat: define interface do repository de user

--- src/User.ts
+++ src/User.ts
@@ -1,10 +1,12 @@
 import { uuid } from "uuidv4";
 
 export class User {
+
   public id: string;
   public name: string;
   public email: string;
   public password: string;
+  
   constructor(props: Omit<User, "id">, id?: string) {
     Object.assign(this, props);
     if (!id) {
