98ecf4ec214b1ecf2e26b1bc8a6a4fcb771f263a
Arthur Vinicius Ribeiro Silva <arthur.ribeiro_badaro@prestador.globo>
2023-05-23 22:36:54 -0300

    test: testes de integracao das rotas de clients

---
+++ test/integration/client.routes.spec.ts
@@ -0,0 +1,27 @@
+import request from "supertest";
+import { App } from "../../src/App";
+
+describe("Client Routes /clients", () => {
+  let app: App;
+  let api: request.SuperTest<request.Test>;
+
+  beforeAll(() => {
+    app = new App();
+    api = request(app.app);
+  });
+
+  it("deve criar um client", async () => {
+    const clientDTO = { name: "John Doe", email: "john@example.com" };
+
+    const response = await api.post("/clients").send(clientDTO);
+
+    expect(response.status).toBe(201);
+  });
+
+  it("retorna uma lista de clientes", async () => {
+    const response = await api.get("/clients");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeInstanceOf(Array);
+  });
+});
